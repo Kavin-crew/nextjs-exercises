8888cbce13d77b183c8cfab35419a2936c12b71a
Kavin abregana <abreganakavin@gmail.com>
2024-02-23 20:12:41 +0800

    familiarizing route folder and nagivation

---
+++ Next.js From Scratch 2024/notes.tsx
@@ -0,0 +1,13 @@
+// common usecase of next/navigation
+// import {
+//   useRouter,
+//   useParams,
+//   useSearchParams,
+//   usePathname,
+// } from "next/navigation";
+
+// const router = useRouter();
+// const searchParams = useSearchParams();
+// const { id } = useParams();
+// const name = searchParams.get("name");
+// const pathname = usePathname();
