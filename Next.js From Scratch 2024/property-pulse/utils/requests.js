01a7ed2bb6388c0490f75e1d9290a7c0098c854a
Kavin Abregana <abreganakavin@gmail.com>
2024-02-27 01:25:04 +0800

    single property page

--- Next.js From Scratch 2024/property-pulse/utils/requests.js
+++ Next.js From Scratch 2024/property-pulse/utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// Fetch all properties
 async function fetchProperties() {
   try {
     // handle the care where the domain is not available yet
@@ -15,4 +16,20 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+// Fetch single properties
+async function fetchProperty(id) {
+  try {
+    // handle the care where the domain is not available yet
+    if (!apiDomain) return null;
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+    if (!res.ok) throw new Error("Failed to fetch data");
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
